a6b549b142702faee0d953e88aefdb3662e2ce8c
winson <929566854@qq.com>
2022-06-11 12:01:25 +0800

    init project

---
+++ winson-for-consul/src/main/java/com/winson/study/consul/ConsulClientDemoV1.java
@@ -0,0 +1,16 @@
+package com.winson.study.consul;
+
+/**
+ * @author winson
+ * @date 2022/6/11
+ * @desc consul 客户端
+ **/
+public class ConsulClientDemoV1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello consul!");
+
+    }
+
+}
