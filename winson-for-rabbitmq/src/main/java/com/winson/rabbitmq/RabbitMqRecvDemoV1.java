b8b7bfb7935010dff88615b049c2d6e22b0b2d02
winson <929566854@qq.com>
2022-06-18 16:13:51 +0800

    test rabbitmq receive

---
+++ winson-for-rabbitmq/src/main/java/com/winson/rabbitmq/RabbitMqRecvDemoV1.java
@@ -0,0 +1,39 @@
+package com.winson.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author winson
+ * @date 2022/6/18
+ **/
+public class RabbitMqRecvDemoV1 {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        String queueName = "winson-rabbit-mq";
+
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(queueName, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+
+    }
+
+}
